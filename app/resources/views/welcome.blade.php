53ef887ae8ead4b8c3661b89fdcb5a023c4bfffc
Kazunori.Wakamatsu <k_wakamatsu@uluru429.local>
2019-07-29 11:08:12 +0900

    test

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-update
+                    Laravel-update2
                 </div>
 
                 <div class="links">
